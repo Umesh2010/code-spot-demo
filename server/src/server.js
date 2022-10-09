2aa94606687ef403a1760c6db73325864b2d2ef7
Umesh2010 <umesh.panchal501@gmail.com>
2022-10-09 20:43:25 +0530

    test 20

--- server/src/server.js
+++ server/src/server.js
@@ -10,14 +10,17 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(require("./routes/values"));
 app.use(require("./routes/utilities"));
-// app.use(require("./routes/turnservertoken"));
+app.use(require("./routes/turnservertoken"));
 app.use(require("./routes/rooms"));
 // get driver connection
 const dbo = require("./db/conn");
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  let db_connect = dbo.getDb();
+  const peers = await db_connect.collection("peers").find().toArray();
   res.json({
-      message: "Run Success"
+      message: "Run Success",
+      data: peers
   });
 });
 
