a49314186eb799924f2a9ed4c42eb622ae5d0c3d
Umesh2010 <umesh.panchal501@gmail.com>
2022-10-09 23:45:05 +0530

    test 103 complete success

--- server/src/server.js
+++ server/src/server.js
@@ -15,25 +15,26 @@ app.use(require("./routes/rooms"));
 // get driver connection
 const dbo = require("./db/conn");
 
-app.get('/', async (req, res) => {
-  await dbo.connectToServer(async function (err) {
-    if (err) console.error(err);
+// app.get('/', async (req, res) => {
+//   await dbo.connectToServer(async function (err) {
+//     if (err) console.error(err);
 
-    let db_connect = dbo.getDb();
-    console.log(db_connect);
+//     let db_connect = dbo.getDb();
+//     console.log(db_connect);
 
-    const peers = await db_connect.collection("peers").find().toArray();
-    res.json({
-      message: "Run Success",
-      data: peers
-    });
-  });
-});
+//     const peers = await db_connect.collection("peers").find().toArray();
+//     res.json({
+//       message: "Run Success",
+//       data: peers
+//     });
+//   });
+// });
 
 app.listen(port, async () => {
   // perform a database connection when server starts
   await dbo.connectToServer(function (err) {
     if (err) console.error(err);
   });
+  
   console.log(`Server is running on port: ${port}`);
 });
