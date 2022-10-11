87ffc92756a45968e67282c6c457b5d4173c28cc
Umesh2010 <umesh.panchal501@gmail.com>
2022-10-11 21:17:54 +0530

    issue solved

--- server/src/db/conn.js
+++ server/src/db/conn.js
@@ -20,6 +20,12 @@ module.exports = {
   },
 
   getDb: function () {
+    client.connect(function (err, db) {
+      if (db) {
+        _db = db.db("spot");
+        console.log("Successfully connected to MongoDB.");
+      }
+    });
     return _db;
   },
 };
